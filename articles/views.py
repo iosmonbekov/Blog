c9b46bbb04b4277528942e15434a0c06d2ece0f2
iosmonbekov <iosmonbekov666@gmail.com>
2021-01-10 14:05:33 +0600

    Project is finished

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from .models import Article, Comment
+from .models import Article
 from django.http import Http404, HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 
@@ -14,4 +15,24 @@ def detail(request, article_id):
     except:
         raise Http404('Article not found.')
 
-    return render(request, 'articles/detail.html', {'article': a})
+    latest_comments_list = a.comment_set.order_by('-id')[:10]
+
+    return render(request, 'articles/detail.html', {'article': a, 'latest_comments_list': latest_comments_list})
+
+def leave_comment(request, article_id):
+    try:
+        a = Article.objects.get(id = article_id)
+    except:
+        raise Http404('Article not found.')
+
+    a.comment_set.create(author_name = request.POST['name'], comment_text = request.POST['text'])
+
+    return HttpResponseRedirect( reverse('articles:detail', args=(a.id,)) )
+
+def single(request):
+    try:
+        a = Article.objects.order_by('-pub_date')[:1]
+    except:
+        raise Http404('Article not found.')
+
+    return render(request, 'articles/single.html',{'article': a,} )
--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('articles/', include('articles.urls'))
+    path('', include('articles.urls'))
 ]
